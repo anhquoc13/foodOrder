f61a1ec9b2f358e6bb44cf0c1ba643aa8712462b
Dang Anh Quoc <aquoc13.s@gmail.com>
2021-03-30 19:38:03 +0700

    food order

---
+++ FoodOrder/app/src/main/java/com/example/foodorder/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.foodorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btn = (Button) findViewById(R.id.btn2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://goo.gl/maps/NGjWob8DfMPwU2Xx6";
+                Intent intentAdd = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentAdd);
+            }
+        });
+    }
+}
